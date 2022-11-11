abac82b4fc01f93a88e7def82187bd3ec42f42d1
Methoni <116286122+Methoni@users.noreply.github.com>
2022-11-11 16:03:22 +0300

    Заходит на целевой сайт, ходит в поиске до 3 стр.

--- bingbot.js
+++ bingbot.js
@@ -5,28 +5,56 @@
 // @description  Bot for Bing
 // @author       Olga Ratnikova
 // @match        https://www.bing.com/*
+// @match        https://www.auto.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
 let links = document.links;
 let btnS = document.getElementById("search_icon");
-let keywords = ["купля-продажа авто", "автомобили б/у", "автомобили купить"];
+let keywords = ["купля-продажа авто", "автомобили б/у", "автомобили купить","тестовая абракадабра"];
 let keyword = keywords[getRandom(0, keywords.length)];
 
+let bingInput = document.getElementsByName("q")[0];
+let bingNP = document.getElementsByClassName("sb_pagN_bp")[0];
+
 if (btnS !== null) {
-document.getElementsByName("q")[0].value = keyword;
-console.log("Lala");
-  btnS.click();
-}
-else {
+  let i = 0;
+  let timerId = setInterval(()=>{
+    bingInput.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      btnS.click();
+    }
+  },400);
+
+} else if (location.hostname == "auto.ru") {
+  console.log("Мы на целевом сайте!");
+
+} else {
+  let nextBingPage = true;
   for (let i = 0; i < links.length; i++) {
     if (links[i].href.indexOf("auto.ru") !== -1) {
       let link = links[i];
-      console.log("Нашёл строку " + link);
-      link.click();
+      nextBingPage = false;
+      console.log("Нашел строку " + link);
+      setTimeout(()=>{
+        link.click();
+      }, getRandom(1500, 4000));
       break;
     }
+  }
+
+  if (document.querySelector(".sb_pagS").innerText == "3") {
+    nextBingPage = false;
+    location.href = "https://www.bing.com/";
+  }
+  if (nextBingPage) {
+    setTimeout(()=>{
+      bingNP.click();
+    }, getRandom(2000, 4000))
+
   }
 }
 
